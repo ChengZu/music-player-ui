388cd3b9ffe2f2e6968105b855dfcee92e186e26
ChengZu <1351606745@msn.cn>
2019-09-19 22:13:30 +0800

    Update player.js

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-﻿var Player = function() {
+var Player = function() {
 	this.paused = true;
 	this.duration = 0;
 	this.currentTime = 0;
@@ -12,28 +12,47 @@
 	this.onerr = function() {};
 	this.onVolume = function() {};
 	this.dom = null;
+	this.elm = null;
 }
 Player.prototype = {
-	$: function(name, dom) {
+	$: function(name) {
+		this.elm = this.$$(name);
+		return this;
+	},
+	$$: function(name, dom) {
 		if (dom == undefined) dom = this.dom;
-		if (dom.id == name || dom.className == name) return dom;
+		if (dom.id == name || dom.className == name) {
+			return dom;
+		}
 		for (var i = 0; i < dom.childNodes.length; i++) {
 			if (dom.childNodes.item(i).id == name || dom.childNodes.item(i).className == name) return dom.childNodes.item(i);
 			if (dom.childNodes.item(i).childNodes.length > 0) {
-				var find = this.$(name, dom.childNodes.item(i));
-				if (find) return find;
+				var find = this.$$(name, dom.childNodes.item(i));
+				if (find) {
+					return find;
+				}
 			}
 		}
+		
 		return false;
 	},
 
-	$$: function(name) {
-		return document.querySelector(name);
+	on: function(type, fun) {
+		this.elm.addEventListener(type,fun);
+		return this;
 	},
-
+	hide: function() {
+		this.elm.style.display = 'none';
+		return this;
+	},
+	show: function() {
+		this.elm.style.display = 'block';
+		return this;
+	},
+	
 	init: function(name, time) {
 		var html = '<div class="player_control"><div class="player_pop_layer"></div><div class="player_pop_content"><div id="player_play_ctr"><div class="player_pause"id="player_playback_pause"></div></div><div id="player_timer">0:00/0:00</div><div id="player_volume_ctr"><div id="player_volume"><div id="player_slider"><div id="player_volume_loaded"><div id="player_volume_pace"></div></div></div></div><div id="player_volume_icon"><div class="player_speaker_iocn"><div class="player_speaker_iocn_1"></div><div class="player_speaker_iocn_2"></div><div class="player_speaker_iocn_3"></div><div class="player_speaker_iocn_4"></div><div class="player_speaker_iocn_disabled"id="player_speaker_iocn_disabled"></div></div></div></div><div id="player_progress"><div id="player_slider"><div id="player_progress_loaded"><div id="player_progress_pace"></div></div></div></div></div></div>';
-		this.dom = this.$$(name);
+		this.dom = document.querySelector(name);
 		this.dom.innerHTML = html;
 		if(time != undefined){
 			this.duration = time;
@@ -41,14 +60,14 @@ Player.prototype = {
 		}
 
 		var _that = this;
-		this.$('player_play_ctr').addEventListener("click",
+		this.$('player_play_ctr').on("click",
 		function(e) {
 			if (_that.paused) _that.play();
 			else _that.pause();
 		},
 		true);
 
-		this.$('player_volume_icon').addEventListener("click",
+		this.$('player_volume_icon').on("click",
 		function(e) {
 			if (_that.volume > 0) {
 				_that.muteVolume = _that.volume;
@@ -99,11 +118,11 @@ Player.prototype = {
 	},
 	
 	disabled: function() {
-		this.$('player_pop_layer').style.display = 'block';
+		this.$('player_pop_layer').show();
 	},
 	
 	enabled: function() {
-		this.$('player_pop_layer').style.display = 'none';
+		this.$('player_pop_layer').hide();
 	},
 	
 	setDuration: function(time) {
@@ -152,13 +171,13 @@ Player.prototype = {
 	initProgressSlider: function() {
 		var _that = this;
 		var slider = this.$("player_progress");
-		slider.addEventListener("mousemove",
+		slider.on("mousemove",
 		function(e) {
 			if (_that.progressClicked) _that.setProgress(e);
 
 		},
 		true);
-		slider.addEventListener("mousedown",
+		slider.on("mousedown",
 		function(e) {
 			_that.progressClicked = true;
 			if (!_that.paused) _that.setOnPaused = true;
@@ -167,7 +186,7 @@ Player.prototype = {
 
 		},
 		true);
-		slider.addEventListener("mouseup",
+		slider.on("mouseup",
 		function(e) {
 			_that.progressClicked = false;
 			if (_that.setOnPaused) _that.play();
@@ -181,19 +200,19 @@ Player.prototype = {
 	initVolumeSlider: function() {
 		var _that = this;
 		var slider = this.$("player_volume");
-		slider.addEventListener("mousemove",
+		slider.on("mousemove",
 		function(e) {
 			if (_that.volumeClicked) _that.setVolume(e);
 		},
 		true);
-		slider.addEventListener("mousedown",
+		slider.on("mousedown",
 		function(e) {
 			_that.volumeClicked = true;
 			_that.setVolume(e);
 
 		},
 		true);
-		slider.addEventListener("mouseup",
+		slider.on("mouseup",
 		function(e) {
 			_that.volumeClicked = false;
 			_that.setVolume(e);
@@ -213,8 +232,8 @@ Player.prototype = {
 	},
 
 	getSlider: function(name, e) {
-		var slider = this.$(name);
-		var width = this.getwidth(this.$(name));
+		var slider = this.$$(name);
+		var width = this.getwidth(slider);
 		var offsetX = e.clientX - this.getLeft(slider);
 		if (offsetX < 0) offsetX = 0;
 		if (offsetX > width) offsetX = width;
@@ -230,22 +249,22 @@ Player.prototype = {
 	},
 
 	updatePlayButton: function() {
-		if (this.paused) this.$('player_playback_pause').className = "player_pause";
-		else this.$('player_playback_pause').className = "player_playback";
+		if (this.paused) this.$('player_playback_pause').elm.className = "player_pause";
+		else this.$('player_playback_pause').elm.className = "player_playback";
 	},
 
 	updateTimerUI: function() {
-		this.$("player_timer").innerHTML = this.formatTime(Math.round(this.currentTime)) + " / " + this.formatTime(Math.round(this.duration));
+		this.$("player_timer").elm.innerHTML = this.formatTime(Math.round(this.currentTime)) + " / " + this.formatTime(Math.round(this.duration));
 	},
 
 	updateProgressUI: function() {
-		this.$("player_progress_loaded").style.width = Math.round((this.currentTime / this.duration) * 100) + "%";
+		this.$("player_progress_loaded").elm.style.width = Math.round((this.currentTime / this.duration) * 100) + "%";
 	},
 
 	updateVolumeUI: function() {
-		this.$("player_volume_loaded").style.width = this.volume + "%";
-		if (this.volume == 0) this.$('player_speaker_iocn_disabled').style.display = 'block';
-		else this.$('player_speaker_iocn_disabled').style.display = 'none';
+		this.$("player_volume_loaded").elm.style.width = this.volume + "%";
+		if (this.volume == 0) this.$('player_speaker_iocn_disabled').show();
+		else this.$('player_speaker_iocn_disabled').hide();
 	},
 
 	formatTime: function(sec) {
